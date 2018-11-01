e25f0cc1f1af9b536addf4c147de799d4f982f6e
xiaole0218 <lvxiaole1995@163.com>
2018-11-01 10:49:01 +0800

    登陆页面渲染-样式调整

---
+++ src/views/login.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="login-wrap">
+  <el-form class="login-form" label-position="top" label-width="80px">
+    <el-form-item label="用户名">
+      <el-input v-model="formdata.username"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="formdata.password"></el-input>
+    </el-form-item>
+    <el-button class="login-button" type="primary">登陆</el-button>
+  </el-form>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "login",
+      data() {
+          return {
+            formdata: {
+              username: '',
+              password: ''
+            }
+          }
+      }
+    }
+</script>
+
+<style scoped>
+  .login-wrap {
+    background-color: #324152;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .login-wrap .login-form {
+    background-color: #fff;
+    width: 400px;
+    padding: 30px;
+    border-radius: 5px;
+  }
+
+  .login-wrap .login-form .login-button {
+    width: 100%;
+  }
+</style>

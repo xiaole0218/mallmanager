17efa969563e9ca629dad238ec58bd9f64b0d701
xiaole0218 <lvxiaole1995@163.com>
2018-11-01 18:12:08 +0800

    搜索框渲染和布局

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>用户组件</div>
+  <!--面包屑-->
+  <el-card class="box-card">
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item>首页</el-breadcrumb-item>
+    <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+    <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+  </el-breadcrumb>
+  <!--搜索框-->
+    <el-row class="searchArea">
+      <el-col :span="24">
+    <el-input placeholder="请输入内容"  class="searchInput">
+      <el-button slot="append" icon="el-icon-search"></el-button>
+    </el-input>
+    <el-button type="success">添加用户</el-button>
+      </el-col>
+    </el-row>
+  <!--表格-->
+  <!--分页-->
+  </el-card>
 </template>
 
 <script>
@@ -9,5 +27,14 @@
 </script>
 
 <style scoped>
-
+  .box-card{
+    height: 100%;
+  }
+  .searchArea{
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  .searchArea .searchInput{
+    width: 350px;
+  }
 </style>

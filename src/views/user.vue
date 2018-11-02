ceb96e9a8953e478d48cb29bb8fb32d9eea785e0
xiaole0218 <lvxiaole1995@163.com>
2018-11-02 19:10:37 +0800

    列表添加功能完成

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,23 +1,48 @@
 <template>
   <!--面包屑-->
-  <el-card class="box-card">
-  <el-breadcrumb separator="/">
-    <el-breadcrumb-item>首页</el-breadcrumb-item>
-    <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-    <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-  </el-breadcrumb>
-  <!--搜索框-->
+  <el-card class="box-card" v-loading="loading"
+  >
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item>首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!--搜索框-->
     <el-row class="searchArea">
       <el-col :span="24">
-    <el-input placeholder="请输入内容"  class="searchInput" v-model="searchVal">
-      <el-button slot="append" icon="el-icon-search" @click="checkUser()"></el-button>
-    </el-input>
-    <el-button type="success">添加用户</el-button>
+        <el-input placeholder="请输入内容" class="searchInput" v-model="searchVal">
+          <el-button slot="append" icon="el-icon-search" @click="checkUser()"></el-button>
+        </el-input>
+        <el-button type="success" @click="showAddUserDia()">添加用户</el-button>
       </el-col>
     </el-row>
-  <!--表格-->
+    <!--添加用户的对话框-->
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdduser">
+      <el-form :model="formData">
+        <el-form-item label="用户名" :label-width="formLabelWidth">
+          <el-input v-model="formData.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input v-model="formData.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" :label-width="formLabelWidth">
+          <el-input v-model="formData.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" :label-width="formLabelWidth">
+          <el-input v-model="formData.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser()">确 定</el-button>
+      </div>
+    </el-dialog>
+
+
+
+
+    <!--表格-->
     <el-table
-      v-loading="loading"
       :data="list"
       style="width: 100%">
       <el-table-column
@@ -59,7 +84,7 @@
           <!--scope.row  就是list 中的每个元素 这里是{}-->
           <!--scope.row.mg_state-->
           <el-switch
-            @change = "changeSwitchMgstate(scope.row)"
+            @change="changeSwitchMgstate(scope.row)"
             v-model="scope.row.mg_state"
             active-color="#13ce66"
             inactive-color="#ff4949">
@@ -71,14 +96,15 @@
         label="操作"
         width="140">
         <template slot-scope="scope">
-            <el-button type="primary" icon="el-icon-edit" size="mini" plain circle></el-button>
-            <el-button type="success" icon="el-icon-check" size="mini" plain circle></el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini" plain circle @click="showDeleBox(scope.row.id)"></el-button>
+          <el-button type="primary" icon="el-icon-edit" size="mini" plain circle></el-button>
+          <el-button type="success" icon="el-icon-check" size="mini" plain circle></el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini" plain circle
+                     @click="showDeleBox(scope.row.id)"></el-button>
         </template>
       </el-table-column>
 
     </el-table>
-  <!--分页-->
+    <!--分页-->
     <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
@@ -91,110 +117,149 @@
 </template>
 
 <script>
-    export default {
-        name: "user",
-      data() {
-        return {
-          list: [],
-          //加载动画的值
-          loading: false,
-          currentPage: 1,
-          total: 0,
-          pagesize: 2,
-          pagenum: 1,
-          //搜索关键字
-          searchVal: ''
-        }
-      },
-      // created  页面出现之前
+  export default {
+    name: "user",
+    data() {
+      return {
+        list: [],
+        //加载动画的值
+        loading: false,
+        currentPage: 1,
+        total: 0,
+        pagesize: 2,
+        pagenum: 1,
+        //搜索关键字
+        searchVal: '',
+        //添加用户添加框的属性
+        dialogFormVisibleAdduser: false,
+        formData: {
+          username: '',
+          password: '',
+          email: '',
+          mobile: ''
+        },
+        //对话框input 的宽度
+        formLabelWidth: '120px'
+      }
+    },
+    // created  页面出现之前
 
-      mounted() {
+    mounted() {
 
-          this.loadTableDate();
+      this.loadTableDate();
+    },
+    methods: {
+      //添加用户
+      async addUser() {
+        //关闭对话框
+        this.dialogFormVisibleAdduser = false
+        //发送请求
+        const res = await this.$http.post('users',this.formData)
+        // console.log(res)
+        const {meta: {status,msg}} = res.data
+        //刷新视图
+        this.loadTableDate()
+        //请求框
+        this.$message.success(msg)
+        //清空表单
+        this.formData = {}
+
       },
-      methods: {
-          //显示删除提示框
-        showDeleBox(userId) {
-          this.$confirm('皇上大人，确认要删除么？', '提示', {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
-            type: 'warning'
-          }).then( async () => {
-           const res = await this.$http.delete(`users/${userId}`)
-            // console.log(res)
+      //显示添加的对话框
+      showAddUserDia() {
+        this.dialogFormVisibleAdduser = true
+      },
+      //显示删除提示框
+      showDeleBox(userId) {
+        this.$confirm('皇上大人，确认要删除么？', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(async () => {
+          const res = await this.$http.delete(`users/${userId}`)
+          // console.log(res)
+          const {meta: {msg, status}} = res.data
+          if (status === 200) {
+            // 刷新表格
+            this.loadTableDate()
+            //
             this.$message({
               type: 'success',
-              message: '删除成功!'
+              message: msg
             });
-          }).catch(() => {
-            this.$message({
-              type: 'info',
-              message: '已取消删除'
-            });
+          }
+
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: msg
           });
-        },
-          //改变用户状态
-        async changeSwitchMgstate (user) {
-          // users/:uId/state/:type
-          //  :uid ->  user.id
-          // : type -> true  或者 false
-          const res = await this.$http.put(`users/${user.id}/state/${user.mg_state}`)
-          // console.log(res)
-          const {meta: {status,msg}} = res.data
-          if (status === 200) {
-            this.$message.success(msg)
-          }
-        },
-          //查询用户
-        checkUser () {
-            this.loadTableDate()
-        },
-          //分页相关方法
-        handleSizeChange(val) {
-          this.pagesize = val
-          this.loadTableDate()
-          // console.log(`每页 ${val} 条`);
-        },
-        handleCurrentChange(val) {
-          this.pagenum= val
-          this.loadTableDate()
-          // console.log(`当前页: ${val}`);
-        },
-          async loadTableDate() {
-            this.loading = true
-            //除了登陆功能，其他功能的接口都需要加入  token  才有请求的权限
-            //header  添加token
+        });
+      },
+      //改变用户状态
+      async changeSwitchMgstate(user) {
+        // users/:uId/state/:type
+        //  :uid ->  user.id
+        // : type -> true  或者 false
+        const res = await this.$http.put(`users/${user.id}/state/${user.mg_state}`)
+        // console.log(res)
+        const {meta: {status, msg}} = res.data
+        if (status === 200) {
+          this.$message.success(msg)
+        }
+      },
+      //查询用户
+      checkUser() {
+        this.loadTableDate()
+      },
+      //分页相关方法
+      handleSizeChange(val) {
+        this.pagesize = val
+        this.loadTableDate()
+        // console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        this.pagenum = val
+        this.loadTableDate()
+        // console.log(`当前页: ${val}`);
+      },
+      async loadTableDate() {
+        this.loading = true
+        //除了登陆功能，其他功能的接口都需要加入  token  才有请求的权限
+        //header  添加token
 
-            const AUTH_TOKEN = sessionStorage.getItem('token')
-            this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-           const res = await this.$http.get(`users?pagenum=${this.pagenum}&pagesize=${this.pagesize}&query=${this.searchVal}`)
-            // console.log(res)
+        const AUTH_TOKEN = sessionStorage.getItem('token')
+        this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+        const res = await this.$http.get(`users?pagenum=${this.pagenum}&pagesize=${this.pagesize}&query=${this.searchVal}`)
+        // console.log(res)
 
-            this.total = res.data.data.total
-            // console.log(this.total)
-            const {meta: {msg, status}, data: {users}} = res.data
-            if (status ===200) {
-              this.loading = false
-              //分页请求后重置页,让搜索的内容在第一页显示
-              this.pagenum = 1
+        this.total = res.data.data.total
+        // console.log(this.total)
+        const {meta: {msg, status}, data: {users}} = res.data
+        if (status === 200) {
+          this.loading = false
+          //分页请求后重置页,让搜索的内容在第一页显示
+          this.pagenum = 1
 
-              this.list = users
-              // console.log(this.list)
-            }
-          }
+          this.list = users
+          // console.log(this.list)
+        }
       }
     }
+  }
 </script>
 
 <style scoped>
-  .box-card{
+  .box-card {
     height: 100%;
   }
-  .searchArea{
+
+  .searchArea {
     margin-top: 10px;
     margin-bottom: 10px;
   }
-  .searchArea .searchInput{
+
+  .searchArea .searchInput {
     width: 350px;
   }
 </style>

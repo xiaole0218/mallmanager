f681e4481f6f6071c02ac07ef67c3efa59465502
xiaole0218 <lvxiaole1995@163.com>
2018-11-03 15:54:45 +0800

    权限管理-拦截器-设置请求头完成

--- src/pulign/http.js
+++ src/pulign/http.js
@@ -3,6 +3,28 @@ const HttpTool = {}
 
 HttpTool.install = (Vue) => {
   axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+
+  axios.interceptors.request.use(function (config) {
+    // console.log('------')
+    // console.log(config)
+    const AUTH_TOKEN = sessionStorage.getItem('token')
+    config.headers.common['Authorization'] = AUTH_TOKEN;
+    // 在发送请求之前做些什么
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
+// 添加响应拦截器
+  axios.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    return response;
+  }, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
   Vue.prototype.$http = axios
+
 }
 export default HttpTool
--- src/views/rights.vue
+++ src/views/rights.vue
@@ -4,6 +4,26 @@
   <!--<rights level1="列表权限" level2="你好"></rights>-->
 <el-card class="box-card">
   <my-bread level1="权限管理" level2="权限列表"></my-bread>
+  <!--表格-->
+  <el-table
+    :data="list"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+  </el-table>
 
 </el-card>
 </template>
@@ -11,6 +31,17 @@
 <script>
     export default {
         name: "rights",
+      created() {
+          this.loadTableData()
+      },
+      methods: {
+          async loadTableData() {
+            // rights/:type
+            //一次性设置授权  = token
+            const res = await this.$http.get(`rights/list`)
+            console.log(res)
+          }
+      }
       
 
     }
